2c76f4ae6a0e5f364291a9ca6df1ba1feee2e9af
Ben King <ben.kingsy@gmail.com>
2018-01-23 14:41:47 +0000

    build: remove rollup-plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
@@ -11,17 +10,6 @@ export default {
   },
   plugins: [
     peerDepsExternal(),
-    commonjs({
-      include: [
-        'node_modules/fbjs/**',
-        'node_modules/object-assign/**',
-        'node_modules/react/**',
-        'node_modules/react-dom/**',
-        'node_modules/prop-types/**',
-        'node_modules/create-react-class/**',
-        'node_modules/@storybook/**',
-      ],
-    }),
     resolve({
       module: true,
       main: true,
