d8e8d5667f186bd325a35dec079d96a2e662447f
Ben King <ben.kingsy@gmail.com>
2018-01-23 16:54:27 +0000

    build: add styled-components

--- src/components/ExampleComponent/index.jsx
+++ src/components/ExampleComponent/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// Styles
+import { Wrapper } from './styles';
+
 type Props = {
   name: string,
 };
 
 export default function ExampleComponent({ name }: Props) {
-  return <div>Hi {name}.</div>;
+  return <Wrapper>Hi {name}.</Wrapper>;
 }
