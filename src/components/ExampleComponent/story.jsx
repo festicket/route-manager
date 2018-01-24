3fa09c5462f90afbf8f91b81302f92f8a3392958
Ben King <ben.kingsy@gmail.com>
2018-01-24 19:04:03 +0000

    build: upgrade @festicket/react-generators

--- src/components/ExampleComponent/story.jsx
+++ src/components/ExampleComponent/story.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ExampleComponent from './';
 
-storiesOf('ExampleComponent', module)
+storiesOf('Components / ExampleComponent', module)
   .add('default', () => (
     <ExampleComponent />
   ));
