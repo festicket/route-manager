cfa211f881fae63b867ebe9a33919d7788269a60
Ben King <ben.kingsy@gmail.com>
2018-01-25 17:33:07 +0000

    build: export theme

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { Grid, GridElement } from './components/layout/Grid/index';
 export { default as breakpoint } from './breakpoints/index';
 export { default as spacing } from './spacing/index';
+export { default as theme } from './theme';
