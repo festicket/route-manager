53b4212e3b669f12ce13776e9675668c952e1f16
Ben King <ben.kingsy@gmail.com>
2018-01-25 18:43:37 +0000

    refactor: change PropTypes for Flow Types

--- src/components/layout/Grid/index.jsx
+++ src/components/layout/Grid/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 import breakpoint from '../../../breakpoints';
 import transformWidthNumber from './utils/transformWidthNumber';
 
 const gap = 30;
 
-const GridElementWidth = props => {
+type BreakpointWidth = number | string;
+
+type BreakpointsShape = {|
+  xs?: BreakpointWidth,
+  sm?: BreakpointWidth,
+  md?: BreakpointWidth,
+  lg?: BreakpointWidth,
+  xlg?: BreakpointWidth,
+|};
+
+type GridElementTypes = {
+  width: BreakpointWidth | BreakpointsShape,
+};
+
+const GridElementWidth = (props: GridElementTypes) => {
   if (typeof props.width === 'object') {
     return Object.entries(props.width).map(bp => {
       const width = transformWidthNumber(bp[1]);
@@ -28,28 +41,22 @@ const GridElementWidth = props => {
   };
 };
 
-Primitive.propTypes = {
-  element: PropTypes.oneOf(['article', 'section', 'div', 'header', 'footer']),
-  children: PropTypes.node,
-};
-
-Primitive.defaultProps = {
-  element: 'div',
-  children: undefined,
-};
-
 // Takes props object but we are only interested in passing className.
 // Otherwise we will get annoying warnings about invalid props on html elements.
-function stripUnwantedProps({ className }) {
+function stripUnwantedProps({ className }: any) {
   return { className };
 }
 
-function Primitive({ element, children, ...props }) {
-  //eslint-disable-line
+type PrimitiveTypes = {
+  element: 'article' | 'section' | 'div' | 'header' | 'footer',
+  children: any,
+};
+
+function Primitive({ element = 'div', children, ...props }: PrimitiveTypes) {
   return React.createElement(element, stripUnwantedProps(props), children);
 }
 
-export const GridElement = styled(Primitive)`
+const StyledGridElement = styled(Primitive)`
   padding: ${gap / 2}px 0;
   ${breakpoint('from-sm')`
     padding: ${gap / 2}px;
@@ -57,30 +64,9 @@ export const GridElement = styled(Primitive)`
   ${GridElementWidth};
 `;
 
-const breakpointWidthType = PropTypes.oneOfType([
-  PropTypes.number,
-  PropTypes.string,
-]);
-
-const breakpointsShape = PropTypes.shape({
-  xs: breakpointWidthType,
-  sm: breakpointWidthType,
-  md: breakpointWidthType,
-  lg: breakpointWidthType,
-  xlg: breakpointWidthType,
-});
-
-GridElement.propTypes = {
-  width: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string,
-    breakpointsShape,
-  ]),
-};
-
-GridElement.defaultProps = {
-  width: 1,
-};
+export const GridElement = (props: GridElementTypes) => (
+  <StyledGridElement {...props} />
+);
 
 export const Grid = styled(Primitive)`
   margin: -${gap / 2}px 0;
