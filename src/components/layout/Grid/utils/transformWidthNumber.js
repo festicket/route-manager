4076d77bf3a08cc8c31971eff33ffe80869dd299
Ben King <ben.kingsy@gmail.com>
2018-01-26 16:02:20 +0000

    style: add return type to transformWidthNumber

--- src/components/layout/Grid/utils/transformWidthNumber.js
+++ src/components/layout/Grid/utils/transformWidthNumber.js
@@ -1,3 +1,3 @@
-export default function transformWidthNumber(w: number) {
+export default function transformWidthNumber(w: number): string {
   return w >= 0 && w <= 1 ? `${w * 100}%` : `${w}px`;
 }
