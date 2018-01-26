27297951236b0811df2de177f2fac3a4a60985ad
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-01-26 15:56:52 +0000

    fix: add theme provider to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,14 @@
-import { configure } from '@storybook/react';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import { ThemeProvider } from "styled-components";
+import theme from "../src/utils/theme";
 
-const req = require.context('../src/', true, /story\.jsx$/);
+const req = require.context("../src/", true, /story\.jsx$/);
+
+addDecorator(story => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
 
 function loadStories() {
-  req.keys().forEach(req);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
