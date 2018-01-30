f04a585a815520f8673f725579b67ce86a18aa01
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-01-30 18:08:12 +0000

    fix: casing of folder directory

---
+++ src/components/text/BodyText/tests/index.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { BodyText } from '@festicket/react-ui-components';
+
+describe('<BodyText />', () => {
+  test('that it renders correctly', () => {
+    const tree = mount(<BodyText size="regular">Regular size</BodyText>);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('that it renders correctly with element prop', () => {
+    const tree = mount(<BodyText element="span">Small size</BodyText>);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('that it renders correctly with spacing prop', () => {
+    const tree = mount(<BodyText spacing="md">Spacing Test</BodyText>);
+    expect(tree).toMatchSnapshot();
+  });
+});
