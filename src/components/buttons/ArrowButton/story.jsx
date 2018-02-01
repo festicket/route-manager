bcd102d0de14a214f54938b957dd1c8428d64895
Ben King <ben.kingsy@gmail.com>
2018-02-01 14:27:17 +0000

    fix: remove redundant element from ArrowButton

--- src/components/buttons/ArrowButton/story.jsx
+++ src/components/buttons/ArrowButton/story.jsx
@@ -3,8 +3,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import ArrowButton from './';
-import Anchor from '../../text/Anchor';
 
 storiesOf('Components / Buttons / ArrowButton', module).add('default', () => (
-  <ArrowButton element={Anchor} to="https://www.google.com" external />
+  <ArrowButton to="https://www.google.com" external />
 ));
--- src/components/buttons/ArrowButton/tests/index.test.jsx
+++ src/components/buttons/ArrowButton/tests/index.test.jsx
@@ -3,13 +3,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ArrowButton from '../';
-import Anchor from '../../../text/Anchor';
 
 describe('<ArrowButton />', () => {
   test('with initial props', () => {
-    const tree = shallow(
-      <ArrowButton element={Anchor} href="/magazine" external />,
-    );
+    const tree = shallow(<ArrowButton href="/magazine" external />);
     expect(tree).toMatchSnapshot();
   });
 });
