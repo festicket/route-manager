aa307bbda832fd65f7cc60f4ec462298b48f7fe0
Ben King <ben.kingsy@gmail.com>
2018-02-01 14:35:49 +0000

    test: add custom element test for ArrowButton

--- src/components/buttons/ArrowButton/tests/index.test.jsx
+++ src/components/buttons/ArrowButton/tests/index.test.jsx
@@ -1,12 +1,23 @@
 // @flow
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ArrowButton from '../';
+
+function El({ children, to }: { children?: any, to: string }) {
+  return <abbr title={to}>{children}</abbr>;
+}
 
 describe('<ArrowButton />', () => {
   test('with initial props', () => {
     const tree = shallow(<ArrowButton href="/magazine" external />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with custom element', () => {
+    const tree = mount(
+      <ArrowButton element={El} to="https://www.google.com" />,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
