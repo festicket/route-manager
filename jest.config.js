693b79690b1ce75fb51dc9ad20e0d2f94bcf87a1
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-02-05 18:16:20 +0000

    fix: update release ci script and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
-const { IS_CI } = process.env;
+// @flow
+const { BRANCH_NAME } = process.env;
 
 module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
   moduleNameMapper: {
     '@festicket/react-ui-components': `<rootDir>/${
-      IS_CI ? 'dist' : 'src'
+      BRANCH_NAME === 'master' ? 'dist' : 'src'
     }/index.js`,
   },
 };
