1a19ff956d4becbcec57c78b5a37dde82701a7be
Jon Parsons <jon@ctrlxctrlv.co.uk>
2018-02-06 13:31:29 +0000

    ci: update jest config to run tests on dist/index only for deploy

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 // @flow
-const { BRANCH_NAME } = process.env;
+const { SEMAPHORE_SERVER_NAME } = process.env;
 
 module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
   moduleNameMapper: {
     '@festicket/react-ui-components': `<rootDir>/${
-      BRANCH_NAME === 'master' ? 'dist' : 'src'
+      SEMAPHORE_SERVER_NAME ? 'dist' : 'src'
     }/index.js`,
   },
 };
