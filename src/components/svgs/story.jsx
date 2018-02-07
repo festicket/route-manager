359887e8d590f01556daf5485a1436fbf0ceacfb
Sam Grace <sam@festicket.com>
2018-02-07 21:00:09 +0000

    refactor: improve svg story code

--- src/components/svgs/story.jsx
+++ src/components/svgs/story.jsx
@@ -11,7 +11,24 @@ const CenterDecorator = storyFn => (
 );
 stories.addDecorator(CenterDecorator);
 
-// Dynamically import SVGs and turn them into stories showing all their valid variations
+// Dynamically import all svg components and generate stories showing all their valid variations
+
+const logoSvgComponents = importAll.sync(`./generated/logo/**/*.js`);
+makeStories(
+  logoSvgComponents,
+  logoSizeColorAndHoverVariationTemplate(
+    [DARK, BRAND, LIGHT],
+    [LOGO_SMALL, LOGO_BIG],
+  ),
+);
+
+const functionalSvgComponents = importAll.sync(
+  `./generated/functional/**/*.js`,
+);
+makeStories(
+  functionalSvgComponents,
+  colorAndHoverVariationTemplate([LIGHT, DARK, BRAND]),
+);
 
 const socialSvgComponents = importAll.sync(`./generated/social/**/*.js`);
 makeStories(socialSvgComponents, colorAndHoverVariationTemplate([LIGHT, DARK]));
@@ -29,83 +46,71 @@ const paymentSvgComponents = importAll.sync(
 );
 makeStories(paymentSvgComponents, componentOnlyTemplate);
 
-const functionalSvgComponents = importAll.sync(
-  `./generated/functional/**/*.js`,
-);
-makeStories(
-  functionalSvgComponents,
-  colorAndHoverVariationTemplate([LIGHT, DARK, BRAND]),
-);
-
-const logoSvgComponents = importAll.sync(`./generated/logo/**/*.js`);
-makeStories(
-  logoSvgComponents,
-  logoSizeColorAndHoverVariationTemplate(
-    [DARK, BRAND, LIGHT],
-    [LOGO_SMALL, LOGO_BIG],
-  ),
-);
-
 /**
- * Dynamically generate stories for each component in the components object
- * using the provided template generator function.
+ * Dynamically generate stories for each default export ReactElement in each module in the modules
+ * argument, using the provided storyGenerator function.
  *
- * @param components {object} - An object whose keys are paths to modules, and whose values are the modules
- * @param storyGenerator {function} - A function that takes a module that exports a default React component.
- * It returns a React Component to be used as the story for the component argument.
+ * @param modules {object} - contains all the modules imported by a call to importAll.sync
+ * An object whose keys are paths to the module, and whose values are the imported bindings.
+ * Each binding is expected to have a default export which is a ReactElement.
+ *
+ * @param storyGenerator {function} - function that takes a ReactElement and returns a
+ * ReactElement that consists of all the variations of the input ReactElement
  */
-function makeStories(components, storyGenerator) {
-  Object.keys(components).forEach(key => {
-    const storyName = key
+function makeStories(modules, storyGenerator) {
+  Object.keys(modules).forEach(path => {
+    const storyName = path
       .split('/') // split path
       .slice(2) // remove '.' and 'generated'
       .map(str => str.replace(/(?!^[A-Z])([A-Z])/g, ' $1')) // PascalCase to 'Normal' Case
       .join(' / ') // put the path back together with spaces between the folders
       .replace(/\.(js$)|(jsx$)/, ''); // remove the file extension form the path
-    stories.add(storyName, () => storyGenerator(components[key]));
+    stories.add(storyName, () => storyGenerator(modules[path].default));
   });
 }
 
 /**
- * Story generator that will generate the default Component of the module with no variations
+ * Story generator that will simply return the passed in ReactElement
  *
- * @param Module
+ * @param ReactElement
  * @returns {ReactElement}
  */
-function componentOnlyTemplate(Module) {
-  return <Module.default />;
+function componentOnlyTemplate(ReactElement) {
+  return <ReactElement />;
 }
 
 /**
- * Returns a story generator that will generate a story for every colour in colorVariants with a
- * hoverable and a non-hoverable variant
+ * Returns a story generator function that takes a ReactElement and will generate a ReactElement
+ * containing every combination of colour in colorVariants and hoverable and non-hoverable variants
+ * of the input ReactElement.
  *
  * @param colorVariants {[...string]}
- * @returns {ReactElement}
+ * @returns {function}
  */
 function colorAndHoverVariationTemplate(colorVariants) {
-  return Module =>
+  return ReactElement =>
     colorVariants.map(color => (
       <div key={color}>
         <h3>color=&quot;{color}&quot; hoverable=&quot;true&quot;</h3>
-        <Module.default color={color} hoverable="true" />
+        <ReactElement color={color} hoverable="true" />
 
         <h3>color=&quot;{color}&quot;</h3>
-        <Module.default color={color} />
+        <ReactElement color={color} />
       </div>
     ));
 }
 
 /**
- * Returns a story generator that will generate a story for every combination of
- * colour in colorVariants, size in sizeVariants and hoverable and non-hoverable variant
+ * Returns a story generator function that takes a ReactElement and will generate a ReactElement
+ * containing every combination of colour in colorVariants, size in sizeVariants and hoverable and
+ * non-hoverable variants of the input ReactElement.
  *
  * @param colorVariants {[...string]}
  * @param sizeVariants {[...string]}
- * @returns {ReactElement}
+ * @returns {function}
  */
 function logoSizeColorAndHoverVariationTemplate(colorVariants, sizeVariants) {
-  return Module =>
+  return ReactElement =>
     colorVariants.map(color =>
       sizeVariants.map(size => (
         <div key={color + size}>
@@ -113,12 +118,12 @@ function logoSizeColorAndHoverVariationTemplate(colorVariants, sizeVariants) {
             color=&quot;{color}&quot; size=&quot;{size}&quot;
             hoverable=&quot;true&quot;
           </h3>
-          <Module.default color={color} size={size} hoverable="true" />
+          <ReactElement color={color} size={size} hoverable="true" />
 
           <h3>
             color=&quot;{color}&quot; size=&quot;{size}&quot;
           </h3>
-          <Module.default color={color} size={size} />
+          <ReactElement color={color} size={size} />
         </div>
       )),
     );
