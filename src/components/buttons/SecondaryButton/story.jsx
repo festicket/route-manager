ce13dcd59c21f0b2702ee35e735d74444d66053d
Ben King <ben.kingsy@gmail.com>
2018-02-07 14:30:36 +0000

    refactor: remove unneeded stories

--- src/components/buttons/SecondaryButton/story.jsx
+++ src/components/buttons/SecondaryButton/story.jsx
@@ -6,15 +6,10 @@ import SecondaryButton from './';
 
 storiesOf('Components / Buttons / SecondaryButton', module)
   .add('Default', () => <SecondaryButton>Button</SecondaryButton>)
-  .add('Default White Background', () => (
-    <div style={{ backgroundColor: 'white' }}>
-      <SecondaryButton>Button</SecondaryButton>
-    </div>
-  ))
   .add('Default Small', () => (
     <SecondaryButton size="small">Button</SecondaryButton>
   ))
-  .add('Default - with isDisabled', () => (
+  .add('Default Small with isDisabled', () => (
     <div style={{ backgroundColor: 'white' }}>
       <SecondaryButton size="small" element="button" isDisabled>
         Disabled Button
