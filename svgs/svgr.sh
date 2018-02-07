c96d9224c6bf61796c8f1d51608db5c4c3b017d1
Sam Grace <sam@festicket.com>
2018-02-07 18:29:34 +0000

    fix: use trailing commas in generated svg components
    these were getting added in after generation by eslint-prettier, but better to add them when generated.

--- svgs/svgr.sh
+++ svgs/svgr.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 rm -rf src/components/svgs/generated
-svgr --single-quote svgs/functional      --template svgs/svg.functional.template.js      --out-dir src/components/svgs/generated/functional
-svgr --single-quote svgs/social          --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/social
-svgr --single-quote svgs/trust-companies --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/trust-companies
-svgr --single-quote svgs/payment-types   --template svgs/svg.payment-types.template.js   --out-dir src/components/svgs/generated/payment-types
-svgr --single-quote svgs/logo            --template svgs/svg.logo.template.js            --out-dir src/components/svgs/generated/logo
+svgr --single-quote --trailing-comma all svgs/functional      --template svgs/svg.functional.template.js      --out-dir src/components/svgs/generated/functional
+svgr --single-quote --trailing-comma all svgs/social          --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/social
+svgr --single-quote --trailing-comma all svgs/trust-companies --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/trust-companies
+svgr --single-quote --trailing-comma all svgs/payment-types   --template svgs/svg.payment-types.template.js   --out-dir src/components/svgs/generated/payment-types
+svgr --single-quote --trailing-comma all svgs/logo            --template svgs/svg.logo.template.js            --out-dir src/components/svgs/generated/logo
