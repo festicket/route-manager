cdc68236283bcbc8c91559db7c544ca33b473af8
Sam Grace <sam@festicket.com>
2018-02-08 14:53:26 +0000

    fix: move invalid babel setting to fix rollup build
    move no babel cache setting into storybook webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,12 +2,26 @@
 //
 // Cause Webpack to watch files that are DYNAMICALLY imported by babel plugin `import-all.macro`
 // const ExtraWatchWebpackPlugin = require('extra-watch-webpack-plugin');
-//
-// module.exports = {
-//   plugins: [
-//     new ExtraWatchWebpackPlugin({
-//       dirs: ['../src/components/svgs/generated'],
-//       files: [],
-//     }),
-//   ],
-// }
+
+module.exports = {
+  // plugins: [
+  //   new ExtraWatchWebpackPlugin({
+  //     dirs: ['../src/components/svgs/generated'],
+  //     files: [],
+  //   }),
+  // ],
+  module: {
+    rules: [
+      {
+        test: /svgs\/story\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            "cacheDirectory": false
+          }
+        }
+      }
+    ]
+  }
+}
