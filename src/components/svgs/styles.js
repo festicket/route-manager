812a1e59cb12a43b608bcc678e4ab05a369d0c86
Sam Grace <sam@festicket.com>
2018-02-12 17:26:53 +0000

    refactor: svg mixins now use styled-tools functions correctly

--- src/components/svgs/styles.js
+++ src/components/svgs/styles.js
@@ -1,25 +1,23 @@
 // @flow
 import { css } from 'styled-components';
-import { switchProp } from 'styled-tools';
+import { switchProp, ifProp } from 'styled-tools';
 
 import { hoverColorMixin } from '../../utils/mixins';
 
 export const LOGO_SMALL = 'small';
 export const LOGO_BIG = 'big';
 
-export const logoHeightVariations = css`
-  height: ${switchProp('size', {
-    [LOGO_SMALL]: '38px',
-    [LOGO_BIG]: '68px',
-  })};
-`;
+export const logoHeightVariations = switchProp('size', {
+  [LOGO_SMALL]: 'height: 38px',
+  [LOGO_BIG]: 'height: 68px',
+});
 
-const hoverStyles = css`
-  cursor: pointer;
-  &:hover {
-    fill: ${hoverColorMixin};
-  }
-`;
-
-export const hoverStylesMixin = (props: { hoverable: string }) =>
-  props.hoverable ? hoverStyles : null;
+export const hoverStylesMixin = ifProp(
+  'hoverable',
+  css`
+    cursor: pointer;
+    &:hover {
+      fill: ${hoverColorMixin};
+    }
+  `,
+);
