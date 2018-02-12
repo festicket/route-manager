cade289a78060d2367631fd53c91070e6a259a18
Sam Grace <sam@festicket.com>
2018-02-12 14:22:13 +0000

    perf: add --no-title to svgr script

--- svgs/svgr.sh
+++ svgs/svgr.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 rm -rf src/components/svgs/generated
-svgr --single-quote --trailing-comma all svgs/functional      --template svgs/svg.functional.template.js      --out-dir src/components/svgs/generated/functional
-svgr --single-quote --trailing-comma all svgs/social          --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/social
-svgr --single-quote --trailing-comma all svgs/trust-companies --template svgs/svg.trust-companies.template.js --out-dir src/components/svgs/generated/trust-companies
-svgr --single-quote --trailing-comma all svgs/payment-types   --template svgs/svg.payment-types.template.js   --out-dir src/components/svgs/generated/payment-types
-svgr --single-quote --trailing-comma all svgs/logo            --template svgs/svg.logo.template.js            --out-dir src/components/svgs/generated/logo
+svgr --single-quote --no-title --trailing-comma all svgs/functional      --template svgs/svg.functional.template.js      --out-dir src/components/svgs/generated/functional
+svgr --single-quote --no-title --trailing-comma all svgs/social          --template svgs/svg.social.template.js          --out-dir src/components/svgs/generated/social
+svgr --single-quote --no-title --trailing-comma all svgs/trust-companies --template svgs/svg.trust-companies.template.js --out-dir src/components/svgs/generated/trust-companies
+svgr --single-quote --no-title --trailing-comma all svgs/payment-types   --template svgs/svg.payment-types.template.js   --out-dir src/components/svgs/generated/payment-types
+svgr --single-quote --no-title --trailing-comma all svgs/logo            --template svgs/svg.logo.template.js            --out-dir src/components/svgs/generated/logo
 
 # export each type of svg from a single file
 folder-module src/components/svgs/generated/functional
