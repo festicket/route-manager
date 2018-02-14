5427553aa5be7afb66ffb21f8be928aa73c64dfa
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-02-14 18:06:04 +0000

    feat: add cypress test for modal component

---
+++ cypress/integration/modal_spec.js
@@ -0,0 +1,20 @@
+// @flow
+
+describe('Modal component Test', () => {
+  beforeEach(() => {
+    cy.visit(
+      'http://localhost:9001/iframe.html?selectedKind=Components%20%2F%20Modal&selectedStory=default&full=1&addons=0&stories=1&panelRight=0&addonPanel=storybook%2Factions%2Fac',
+    );
+    cy.get('#storybook-test').should('not.be.visible');
+    cy.get('button.modal-button').click();
+  });
+
+  it('should open on click', () => {
+    cy.get('#storybook-test').should('be.visible');
+  });
+
+  it('should close the modal on click', () => {
+    cy.get('#modal-close').click();
+    cy.get('#storybook-test').should('not.be.visible');
+  });
+});
