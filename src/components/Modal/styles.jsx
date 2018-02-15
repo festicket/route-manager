da5bb46a39da56fedf7c3a9d426882ee0cf3e0dc
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-02-15 18:21:04 +0000

    fix: remove duplciate style rule

--- src/components/Modal/styles.jsx
+++ src/components/Modal/styles.jsx
@@ -63,7 +63,6 @@ export const StyledModal = styled(ReactModalAdapter).attrs({
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
     width: 100vw;
     height: 100%;
     background-color: #242529;
