1a2455c96ac015ac5bdf369f8769f9cce7819759
Ben King <ben.kingsy@gmail.com>
2018-02-15 10:33:08 +0000

    fix: make width prop optional for GridElement

--- src/components/layout/Grid/utils/transformWidthNumber.js
+++ src/components/layout/Grid/utils/transformWidthNumber.js
@@ -1,5 +1,9 @@
 // @flow
 
-export default function transformWidthNumber(w: number): string {
+export default function transformWidthNumber(w: number | void): string {
+  if (w === undefined) {
+    return '100%';
+  }
+
   return w >= 0 && w <= 1 ? `${w * 100}%` : `${w}px`;
 }
