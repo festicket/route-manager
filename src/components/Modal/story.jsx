24e575249662c65182db09c6e9fef1dce9548235
Richie McColl <richie.mccoll@hotmail.co.uk>
2018-02-20 10:00:56 +0000

    fix: update story to have a test where content is long enough to scroll

--- src/components/Modal/story.jsx
+++ src/components/Modal/story.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { Fragment } from 'react';
 import { withState } from 'recompose';
 import { storiesOf } from '@storybook/react';
 import Modal from './';
@@ -25,56 +25,33 @@ function ModalWrapper({ isOpen, toggleModal, children }) {
   );
 }
 
+const dummyText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
+orci viverra, cursus purus ac, condimentum lacus. Cras et mauris
+nulla. Curabitur lectus dolor, vehicula ut ultricies sodales, varius
+rhoncus risus. Morbi rutrum sed tellus nec efficitur. Maecenas dictum
+semper quam non ornare. Quisque interdum sapien ac ex cursus luctus.
+Donec id nisi pellentesque, varius turpis cursus, dictum ipsum.
+Curabitur ipsum justo, scelerisque a magna eget, laoreet pharetra
+lectus. Aenean hendrerit massa non urna viverra ultricies. Nulla
+facilisi. Ut lacinia tortor a justo lobortis aliquam. Nullam at
+vestibulum elit. In molestie justo a fringilla rutrum. Vivamus rhoncus
+lacus vitae nisl porttitor, pellentesque euismod dolor mattis. Cras
+quis diam nisl. Nulla bibendum feugiat commodo.`;
+
+// $FlowFixMe
+const modalContent = Array.from({ length: 8 }).fill(dummyText);
+
 storiesOf('Components / Modal', module).add('default', () => (
   <ModalWithState>
     <Background.White>
       <h1 id="modal-content">Modal Test!</h1>
       <article>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
-          orci viverra, cursus purus ac, condimentum lacus. Cras et mauris
-          nulla. Curabitur lectus dolor, vehicula ut ultricies sodales, varius
-          rhoncus risus. Morbi rutrum sed tellus nec efficitur. Maecenas dictum
-          semper quam non ornare. Quisque interdum sapien ac ex cursus luctus.
-          Donec id nisi pellentesque, varius turpis cursus, dictum ipsum.
-          Curabitur ipsum justo, scelerisque a magna eget, laoreet pharetra
-          lectus. Aenean hendrerit massa non urna viverra ultricies. Nulla
-          facilisi. Ut lacinia tortor a justo lobortis aliquam. Nullam at
-          vestibulum elit. In molestie justo a fringilla rutrum. Vivamus rhoncus
-          lacus vitae nisl porttitor, pellentesque euismod dolor mattis. Cras
-          quis diam nisl. Nulla bibendum feugiat commodo.
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
-          orci viverra, cursus purus ac, condimentum lacus. Cras et mauris
-          nulla. Curabitur lectus dolor, vehicula ut ultricies sodales, varius
-          rhoncus risus. Morbi rutrum sed tellus nec efficitur. Maecenas dictum
-          semper quam non ornare. Quisque interdum sapien ac ex cursus luctus.
-          Donec id nisi pellentesque, varius turpis cursus, dictum ipsum.
-          Curabitur ipsum justo, scelerisque a magna eget, laoreet pharetra
-          lectus. Aenean hendrerit massa non urna viverra ultricies. Nulla
-          facilisi. Ut lacinia tortor a justo lobortis aliquam. Nullam at
-          vestibulum elit. In molestie justo a fringilla rutrum. Vivamus rhoncus
-          lacus vitae nisl porttitor, pellentesque euismod dolor mattis. Cras
-          quis diam nisl. Nulla bibendum feugiat commodo.
-        </p>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
-          orci viverra, cursus purus ac, condimentum lacus. Cras et mauris
-          nulla. Curabitur lectus dolor, vehicula ut ultricies sodales, varius
-          rhoncus risus. Morbi rutrum sed tellus nec efficitur. Maecenas dictum
-          semper quam non ornare. Quisque interdum sapien ac ex cursus luctus.
-          Donec id nisi pellentesque, varius turpis cursus, dictum ipsum.
-          Curabitur ipsum justo, scelerisque a magna eget, laoreet pharetra
-          lectus. Aenean hendrerit massa non urna viverra ultricies. Nulla
-          facilisi. Ut lacinia tortor a justo lobortis aliquam. Nullam at
-          vestibulum elit. In molestie justo a fringilla rutrum. Vivamus rhoncus
-          lacus vitae nisl porttitor, pellentesque euismod dolor mattis. Cras
-          quis diam nisl. Nulla bibendum feugiat commodo.
-        </p>
-        <br />
+        {modalContent.map(item => (
+          <Fragment key={Math.random()}>
+            <p>{item}</p>
+            <br />
+          </Fragment>
+        ))}
       </article>
     </Background.White>
   </ModalWithState>
