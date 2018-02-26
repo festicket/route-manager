4801acb0b433a0e7aaae13f9a4951f97339d22fd
Ben King <ben.kingsy@gmail.com>
2018-02-26 12:34:46 +0000

    docs: document font-size: 0

--- src/components/buttons/styles.jsx
+++ src/components/buttons/styles.jsx
@@ -1,6 +1,12 @@
 // @flow
 import { css } from 'styled-components';
 
+/*
+Font size is reset to 0 on the outer element in order to prevent
+any whitespace between the outer element and the child flex wrapper 
+
+https://stackoverflow.com/questions/5078239/how-do-i-remove-the-space-between-inline-block-elements
+*/
 export default function buttonMixin() {
   return css`
     display: inline-block;
