b10b48c6df4eb0b6eb662e2fa21cb865addbd994
Ben King <ben.kingsy@gmail.com>
2018-02-26 10:50:56 +0000

    fix: button flex in Safari 10.1

--- src/components/buttons/ButtonPrimitive/index.jsx
+++ src/components/buttons/ButtonPrimitive/index.jsx
@@ -1,7 +1,8 @@
 // @flow
 
 import * as React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { switchProp } from 'styled-tools';
 import Anchor from '../../text/Anchor';
 import { blacklistProps } from '../../../utils/props';
 
@@ -14,6 +15,26 @@ type Props = {
   isDisabled?: boolean,
   render?: () => mixed,
 };
+
+const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+
+  ${switchProp('size', {
+    regular: css`
+      min-width: 200px;
+      min-height: 50px;
+      padding: 15px 30px;`,
+    small: css`
+      min-width: 75px;
+      min-height: 40px;
+      padding: 10px 15px;`,
+    inline: css`
+      padding: 0;`,
+  })};
+`;
 
 function Button(props) {
   /*
@@ -34,7 +55,9 @@ function Button(props) {
 
   return (
     <button {...newProps} aria-disabled={isDisabled} disabled={isDisabled}>
-      {resultChildren || props.children}
+      <FlexWrapper size={props.size}>
+        {resultChildren || props.children}
+      </FlexWrapper>
     </button>
   );
 }
@@ -63,5 +86,9 @@ export default function Primitive({
     'isDisabled',
   ]);
 
-  return <Anchor {...newProps} weight="bold" />;
+  return (
+    <Anchor {...newProps} weight="bold">
+      <FlexWrapper size={props.size}>{props.children}</FlexWrapper>
+    </Anchor>
+  );
 }
--- src/components/buttons/RoundedChevronButton/styles.jsx
+++ src/components/buttons/RoundedChevronButton/styles.jsx
@@ -4,14 +4,20 @@ import styled from 'styled-components';
 import { darken, rgba } from 'polished';
 import ButtonPrimitive from '../ButtonPrimitive';
 
+export const FlexWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+`;
+
 export const Button = styled(ButtonPrimitive)`
-  display: inline-flex;
+  display: inline-block;
   align-items: center;
   justify-content: center;
   border-radius: 100%;
   padding: 0;
-  width: 28px;
-  height: 28px;
   cursor: pointer;
   background-color: ${({ theme }) => rgba(theme.colors.greyscale.black, 0.05)};
 
