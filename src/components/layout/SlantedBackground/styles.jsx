f4c1ae1d94a068d83676afc0a537e092db8dd38a
Ben King <ben.kingsy@gmail.com>
2018-02-27 15:25:33 +0000

    refactor: slant offset to use CSS before and after

--- src/components/layout/SlantedBackground/styles.jsx
+++ src/components/layout/SlantedBackground/styles.jsx
@@ -3,22 +3,21 @@
 import styled, { css } from 'styled-components';
 import { prop, ifProp, switchProp } from 'styled-tools';
 
-const defaultHeight = 200;
-const offsetMultiplier = 2;
-/*
-This crazy thing calculates how far down the slant of the polygon should start,
-while retaining the overall angle.
-*/
-const offsetPolygonPosition = `${(1 - 1 / offsetMultiplier) * 100}%`;
+const height = 200;
+const offsetPosition = 200;
 
 export const StyledSlantedBackground = styled.div`
   position: relative;
   overflow: hidden;
+  /*
+  The z-index must be set to 0 in order to create a new stacking context.
+  */
+  z-index: 0;
   min-height: ${ifProp(
     'slantVerticalOffset',
-    `${defaultHeight * offsetMultiplier}px`,
-    `${defaultHeight}px`,
-  )};
+    height + offsetPosition,
+    height,
+  )}px;
   background: ${switchProp('backgroundVariant', {
     none: 'none',
     primary: prop('theme.colors.brand.primary'),
@@ -29,26 +28,44 @@ export const StyledSlantedBackground = styled.div`
     position: relative;
   }
 
+  ${ifProp(
+    'slantVerticalOffset',
+    css`
+      &::after {
+        display: block;
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        height: ${offsetPosition}px;
+        background: ${switchProp('slantVariant', {
+          white: prop('theme.colors.white'),
+          primary: prop('theme.colors.brand.primary'),
+          grey: prop('theme.colors.greyscale.grey'),
+        })};
+        z-index: -1;
+      }
+    `,
+  )};
+
   &::before {
     display: block;
     content: '';
     position: absolute;
-    top: 0;
+    top: ${ifProp('slantVerticalOffset', `${offsetPosition}px`, '0')};
     left: 50%;
     transform: translateX(-50%);
     min-width: 2000px;
     width: 100%;
+    z-index: -1;
 
     /*
     Use of min-width and width preserves the angle of the slant up
     until 2000px, then spans the full width of the user's massive monitor
     */
 
-    height: ${ifProp(
-      'slantVerticalOffset',
-      `${defaultHeight * offsetMultiplier}px`,
-      `${defaultHeight}px`,
-    )};
+    height: ${height}px;
 
     background: ${switchProp('slantVariant', {
       white: prop('theme.colors.white'),
@@ -58,31 +75,15 @@ export const StyledSlantedBackground = styled.div`
 
     -webkit-clip-path: polygon(
       ${switchProp('slantHorizontalDirection', {
-        'to-left': css`0 0, 100% 0, 100% ${ifProp(
-          'slantVerticalOffset',
-          offsetPolygonPosition,
-          '0',
-        )}, 0 100%`,
-        'to-right': css`0 0, 100% 0, 100% 100%, 0 ${ifProp(
-          'slantVerticalOffset',
-          offsetPolygonPosition,
-          '0',
-        )}`,
+        'to-left': '0 0, 100% 0, 100% 0, 0 100%',
+        'to-right': '0 0, 100% 0, 100% 100%, 0 0',
       })}
     );
 
     clip-path: polygon(
       ${switchProp('slantHorizontalDirection', {
-        'to-left': css`0 0, 100% 0, 100% ${ifProp(
-          'slantVerticalOffset',
-          offsetPolygonPosition,
-          '0',
-        )}, 0 100%`,
-        'to-right': css`0 0, 100% 0, 100% 100%, 0 ${ifProp(
-          'slantVerticalOffset',
-          offsetPolygonPosition,
-          '0',
-        )}`,
+        'to-left': '0 0, 100% 0, 100% 0, 0 100%',
+        'to-right': '0 0, 100% 0, 100% 100%, 0 0',
       })}
     );
 
