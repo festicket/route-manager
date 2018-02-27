50161792722f6a90507fef96669aa014040f1a9a
Ben King <ben.kingsy@gmail.com>
2018-02-27 12:26:49 +0000

    fix: break out Wrapper styles

--- src/components/layout/Wrapper/index.jsx
+++ src/components/layout/Wrapper/index.jsx
@@ -1,42 +1,7 @@
 // @flow
 
 import * as React from 'react';
-import styled, { css } from 'styled-components';
-import { switchProp } from 'styled-tools';
-import breakpoint from '../../../utils/breakpoint';
-
-const StyledWrapper = styled.div`
-  ${switchProp('size', {
-    normal: css`
-      max-width: 100%;
-      margin: 0 auto;
-      padding: 0 20px;
-
-      ${breakpoint('from-sm')`
-        padding: 0 50px;
-      `};
-
-      ${breakpoint('from-lg')`
-        width: 1220px;
-      `};`,
-    alternative: css`
-      max-width: 100%;
-      margin: 0 auto;
-      padding: 0 20px;
-
-      ${breakpoint('from-sm')`
-        padding: 0 50px;
-      `};
-
-      ${breakpoint('from-md')`
-        width: 650px;
-      `};
-
-      ${breakpoint('from-lg')`
-        width: 800px;
-      `};`,
-  })};
-`;
+import { StyledWrapper } from './styles';
 
 type Props = {|
   size: 'full' | 'normal' | 'alternative',
