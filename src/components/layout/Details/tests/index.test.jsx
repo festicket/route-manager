cfeafedd53f239042e5dd443a998a9dd84adcf75
Ben King <ben.kingsy@gmail.com>
2018-03-01 12:23:44 +0000

    test: test for button click

--- src/components/layout/Details/tests/index.test.jsx
+++ src/components/layout/Details/tests/index.test.jsx
@@ -1,8 +1,11 @@
 // @flow
 
 import React from 'react';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
 import { Details } from '@festicket/react-ui-components';
 
+import theme from '../../../../utils/theme';
 import { renderWithTheme } from '../../../../utils/testing';
 import { title, body } from './fixtures';
 
@@ -19,5 +22,21 @@ describe('<Details />', () => {
       </Details>,
     );
     expect(tree).toMatchSnapshot();
+  });
+
+  test('should open on button click', () => {
+    const tree = mount(
+      <ThemeProvider theme={theme}>
+        <Details title={title}>{body}</Details>
+      </ThemeProvider>,
+    );
+
+    const button = tree.find('RoundedChevronButton');
+
+    expect(tree.find('BodyWrapper').prop('open')).toBe(false);
+
+    button.simulate('click');
+
+    expect(tree.find('BodyWrapper').prop('open')).toBe(true);
   });
 });
