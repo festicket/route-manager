8b3c8c09cfc035004e6f0a07c6d2c708b9dd962d
Ben King <ben.kingsy@gmail.com>
2018-03-01 12:32:41 +0000

    fix: add BodyText to ParagraphWrapper story

--- src/components/text/ParagraphWrapper/story.jsx
+++ src/components/text/ParagraphWrapper/story.jsx
@@ -6,9 +6,12 @@ import { storiesOf } from '@storybook/react';
 import ParagraphWrapper from './';
 import html from './tests/fixtures';
 import { Background } from '../../../utils/storybook-helpers';
+import BodyText from '../BodyText';
 
 storiesOf('Components / Text / ParagraphWrapper', module).add('default', () => (
   <Background.White>
-    <ParagraphWrapper html={html} />
+    <BodyText element="span">
+      <ParagraphWrapper html={html} />
+    </BodyText>
   </Background.White>
 ));
