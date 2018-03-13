fc74a0d8bbec3f2ee694f9528447a13b9bc33014
Sam Grace <sam@festicket.com>
2018-03-13 09:55:21 +0000

    refactor: add ticket number to comment

--- src/components/buttons/roundedButtons/CloseButton/styles.jsx
+++ src/components/buttons/roundedButtons/CloseButton/styles.jsx
@@ -7,7 +7,7 @@ const CloseStyledButtonPrimitive = StyledButtonPrimitive.extend`
   background-color: ${prop('theme.colors.white')};
   box-shadow: 0 2px 30px 0 ${prop('theme.colors.shadowColor')};
 
-  // TODO: hover styles?
+  // TODO: add hover styles in FES-5617
 
   ${FlexWrapper} {
     width: 38px;
