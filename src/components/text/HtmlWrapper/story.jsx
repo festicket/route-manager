e154d8c6b405ef53018c66e80b793abc32b3d643
Joe Bourne <josephbourne14@gmail.com>
2018-03-19 16:20:27 +0000

    feat: use Background.Documentation in story

--- src/components/text/HtmlWrapper/story.jsx
+++ src/components/text/HtmlWrapper/story.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Background } from 'src/utils/storybook-helpers';
 
 import HtmlWrapper from './';
 import { h2AndParagraphs, oneParagraph } from './tests/fixtures';
@@ -9,15 +10,17 @@ import { h2AndParagraphs, oneParagraph } from './tests/fixtures';
 storiesOf('Components / Text / HtmlWrapper', module)
   .add('H2 and several paragraphs', () => (
     <div>
-      <p>Below this, expect a span containing an h2 and some paragraphs.</p>
-      <hr />
+      <Background.Documentation>
+        <p>Below this, expect a span containing an h2 and some paragraphs.</p>
+      </Background.Documentation>
       <HtmlWrapper htmlString={h2AndParagraphs} />
     </div>
   ))
   .add('One paragraph', () => (
     <div>
-      <p>Below this, expect a span containing one paragraph.</p>
-      <hr />
+      <Background.Documentation>
+        <p>Below this, expect a span containing one paragraph.</p>
+      </Background.Documentation>
       <HtmlWrapper htmlString={oneParagraph} />
     </div>
   ));
