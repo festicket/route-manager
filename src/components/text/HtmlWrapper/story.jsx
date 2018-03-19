8d7ab4da34ba489a1ca44a3861c8d5300f7f417f
Joe Bourne <josephbourne14@gmail.com>
2018-03-19 15:34:18 +0000

    feat: improve docs for HtmlWrapper

--- src/components/text/HtmlWrapper/story.jsx
+++ src/components/text/HtmlWrapper/story.jsx
@@ -9,11 +9,15 @@ import { h2AndParagraphs, oneParagraph } from './tests/fixtures';
 storiesOf('Components / Text / HtmlWrapper', module)
   .add('H2 and several paragraphs', () => (
     <div>
+      <p>Below this, expect a span containing an h2 and some paragraphs.</p>
+      <hr />
       <HtmlWrapper htmlString={h2AndParagraphs} />
     </div>
   ))
   .add('One paragraph', () => (
     <div>
+      <p>Below this, expect a span containing one paragraph.</p>
+      <hr />
       <HtmlWrapper htmlString={oneParagraph} />
     </div>
   ));
