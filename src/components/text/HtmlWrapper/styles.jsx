58605000fb40134b555ef9bbbca5fc6153dddc57
Ben King <ben.kingsy@gmail.com>
2018-03-21 17:23:38 +0000

    fix: update spacing and link styles

--- src/components/text/HtmlWrapper/styles.jsx
+++ src/components/text/HtmlWrapper/styles.jsx
@@ -1,10 +1,35 @@
 // @flow
 
 import styled from 'styled-components';
+import { prop } from 'styled-tools';
 import spacing from 'src/utils/spacing';
 
 export default styled.span`
-  p:not(:last-of-type) {
+  p,
+  ul {
+    margin: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p:not(:last-child),
+  ul:not(:last-child),
+  ol:not(:last-child) {
     ${spacing('md')};
+  }
+
+  li {
+    ${spacing('xs')};
+  }
+
+  a {
+    text-decoration: underline;
+    text-decoration-skip: ink;
+    font-weight: ${prop('theme.fontWeight.regular')};
+    color: ${prop('theme.colors.greyscale.black')};
   }
 `;
