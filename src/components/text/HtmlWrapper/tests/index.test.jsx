f5cff2d3d2eb0dd3c13ed93a5c9b45f146bb7988
Ben King <ben.kingsy@gmail.com>
2018-03-21 17:29:05 +0000

    test: update HtmlWrapper tests

--- src/components/text/HtmlWrapper/tests/index.test.jsx
+++ src/components/text/HtmlWrapper/tests/index.test.jsx
@@ -1,15 +1,25 @@
 // @flow
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { renderWithTheme } from 'src/tests/utils';
 
 import { HtmlWrapper } from '@festicket/react-ui-components';
 
-import { oneParagraph } from './fixtures';
+import { oneParagraph, h2AndParagraphs, variedContent } from './fixtures';
 
 describe('<HtmlWrapper />', () => {
   test('with initial props', () => {
-    const tree = shallow(<HtmlWrapper htmlString={oneParagraph} />);
+    const tree = renderWithTheme(<HtmlWrapper htmlString={oneParagraph} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with h2 and paragraphs', () => {
+    const tree = renderWithTheme(<HtmlWrapper htmlString={h2AndParagraphs} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with varied content', () => {
+    const tree = renderWithTheme(<HtmlWrapper htmlString={variedContent} />);
     expect(tree).toMatchSnapshot();
   });
 });
