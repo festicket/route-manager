ad39e5e626935bc83a74b9ebc1dc75c25d57ab4b
Marco Alabruzzo <marco.alabruzzo@gmail.com>
2018-03-28 18:08:46 +0100

    fix: add responsive space to the Detail component

--- src/components/layout/Details/styles.jsx
+++ src/components/layout/Details/styles.jsx
@@ -3,6 +3,7 @@
 import styled from 'styled-components';
 import { ifProp } from 'styled-tools';
 import Gutter from 'src/components/layout/Gutter';
+import media from 'src/utils/media';
 
 export const StyledGutter = styled(Gutter)`
   display: flex;
@@ -11,6 +12,16 @@ export const StyledGutter = styled(Gutter)`
   cursor: pointer;
   padding-top: 20px;
   padding-bottom: 20px;
+
+  ${media('from-sm')} {
+    padding-top: 25px;
+    padding-bottom: 25px;
+  }
+
+  ${media('from-md')} {
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
 `;
 
 export const BodyWrapper = styled.div`
