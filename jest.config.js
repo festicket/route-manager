379d41fe9cc3f07f218ffa811bf271032ac505a5
Sam Grace <sam@festicket.com>
2018-04-03 17:24:51 +0100

    refactor: ignore tests in build folder

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,6 @@ module.exports = {
       SEMAPHORE_SERVER_NAME ? 'dist' : 'src'
     }/index.js`,
   },
+  // while the build is simply transpiling with Babel to dist, we need to ignore those test files
+  testPathIgnorePatterns: ['<rootDir>/dist/'],
 };
